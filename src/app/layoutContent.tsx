f4ba385eb168b0a99295092907cc51e5e0f22d37
Pedro Trotta <pedroforte1911@gmail.com>
2025-03-19 12:29:15 -0400

    taking off pages mentions

--- src/app/layoutContent.tsx
+++ src/app/layoutContent.tsx
@@ -3,7 +3,6 @@
 import { usePathname } from "next/navigation";
 import { AnimatedTabs } from "@/components/AnimatedTab";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import RegisterPage from "@/app/register/page";
 
 // Create a QueryClient instance (or import it from a separate file like queryClient.ts)
 const queryClient = new QueryClient();
@@ -11,8 +10,7 @@ const queryClient = new QueryClient();
 function LayoutContent({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
-  if (pathname === "/register") {
-    return <RegisterPage />;
+  if (pathname === "/") {
   }
 
   return (
